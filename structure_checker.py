25b1b351e1b64b254a5951fbeeb3feaf00781c5f
yellow-sky <nikulin.e@gmail.com>
2013-07-25 00:53:45 +0400

    Added checks: tik_id is not null and tik_id has uniq name

--- structure_checker.py
+++ structure_checker.py
@@ -24,6 +24,30 @@ class DataStructureChecker():
             print 'Input source can\'t be opened: ' + unicode(gdal.GetLastErrorMsg(), _message_encoding)
         return True
 
+    
+    def check_tik_ids(self, csv_file_path):
+        input_data_source = ogr.Open(csv_file_path.encode('utf-8'))
+        csv_layer = input_data_source[0]
+
+        tik_ids = {}
+        in_feat = csv_layer.GetNextFeature()
+        while in_feat is not None:
+            id = in_feat['tik_id']
+            name = in_feat['tik']
+            if not id:
+                    print '\t Invalid tik ids! Found null id. name = %s' % (name)
+                    return False
+            
+            if tik_ids.has_key(id):
+                if tik_ids[id] != name:
+                    print '\t Invalid tik ids! Found: id = %s, name = %s and id = %s, name = %s' % (id, tik_ids[id], id, name)
+                    return False
+            else:
+                tik_ids[id] = name
+            in_feat = csv_layer.GetNextFeature()
+        
+        return True
+
         
     def check(self, csv_file_path):
         input_data_source = ogr.Open(csv_file_path.encode('utf-8'))
